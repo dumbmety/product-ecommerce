64d06b47fa056ab8b4f5ed94071adbabd94b3340
Mehdi Neysi <dev.mehdineysi@gmail.com>
2021-09-27 12:13:36 +0330

    fix: navigation popovers z-index (#1)

--- components/Navigation/FlyoutMenus.tsx
+++ components/Navigation/FlyoutMenus.tsx
@@ -34,10 +34,10 @@ export default function FlyoutMenus() {
                   leaveFrom="opacity-100"
                   leaveTo="opacity-0"
                 >
-                  <Popover.Panel className="absolute top-full inset-x-0 text-sm text-gray-500">
+                  <Popover.Panel className="absolute z-10 top-full inset-x-0 text-sm text-gray-500">
                     {/* Presentational element used to render the bottom shadow, if we put the shadow on the actual panel it pokes out the top, so we use this shorter element to hide the top of the shadow */}
                     <div
-                      className="absolute inset-0 top-1/2 bg-white shadow"
+                      className="absolute inset-0 top-1/2 bg-white shadow-lg"
                       aria-hidden="true"
                     />
 
